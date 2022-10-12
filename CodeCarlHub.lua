b8c0f4d0ab73ee76043b67f3c542741f69033d71
Carl-png <76801275+Carl-png@users.noreply.github.com>
2022-10-11 23:45:17 -0600

    Add files via upload

---
+++ CodeCarlHub.lua
@@ -0,0 +1,244 @@
+local Material = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kinlei/MaterialLua/master/Module.lua"))()
+
+local X = Material.Load({
+	Title = "CODECARL",
+	Style = 3,
+	SizeX = 500,
+	SizeY = 350,
+	Theme = "Dark",
+	ColorOverrides = {
+		MainFrame = Color3.fromRGB(163,145,147)
+	}
+})
+
+local Y = X.New({
+	Title = "Scripts"
+})
+
+local Z = X.New({
+	Title = "Extras"
+})
+
+local script1 = Y.Button({
+	Text = "Tapping Simulator GUI",
+	Callback = function()
+		loadstring(game:HttpGet("https://arches-systems.com/loader.lua",true))()
+	end,
+	Menu = {
+		Information = function(self)
+			X.Banner({
+				Text = "This function can get you banned in up-to-date servers; use at your own risk."
+			})
+		end
+	}
+})
+
+local script2 = Y.Button({
+	Text = "Anime Adventures",
+	Callback = function()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/TrapstarKSSKSKSKKS/Main/main/TrapHub.lua"))()
+	end,
+	Menu = {
+		Information = function(self)
+			X.Banner({
+				Text = "This function can get you banned in up-to-date servers; use at your own risk."
+			})
+		end
+	}
+})
+
+local script3 = Y.Button({
+	Text = "Universal Silent Aim",
+	Callback = function()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/Averiias/Universal-SilentAim/main/main.lua"))()
+	end,
+	Menu = {
+		Information = function(self)
+			X.Banner({
+				Text = "This function can get you banned in up-to-date servers; use at your own risk."
+			})
+		end
+	}
+})
+
+local script4 = Y.Button({
+	Text = "NO STOP Obby",
+	Callback = function()
+		loadstring(game:HttpGet("https://pastebin.com/raw/cj24bZLZ"))()
+	end,
+	Menu = {
+		Information = function(self)
+			X.Banner({
+				Text = "This function can get you banned in up-to-date servers; use at your own risk."
+			})
+		end
+	}
+})
+
+local script5 = Y.Button({
+	Text = "My Restaurant AutoFarm",
+	Callback = function()
+		loadstring(game:HttpGet("https://rawscripts.net/raw/My-Restaurant!-Fast-Autofarm-4610"))()
+	end,
+	Menu = {
+		Information = function(self)
+			X.Banner({
+				Text = "This function can get you banned in up-to-date servers; use at your own risk."
+			})
+		end
+	}
+})
+
+local script6 = Y.Button({
+	Text = "Gumball Factory Tycoon AutoObby",
+	Callback = function()
+		loadstring(game:HttpGet("https://pastebin.com/raw/9gW2zHN8"))()
+	end,
+	Menu = {
+		Information = function(self)
+			X.Banner({
+				Text = "This function can get you banned in up-to-date servers; use at your own risk."
+			})
+		end
+	}
+})
+
+local script7 = Y.Button({
+	Text = "Gumball Factory Tycoon AutoFarm",
+	Callback = function()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/kinhey/scripts/main/gumball_autofarm.lua"))()
+	end,
+	Menu = {
+		Information = function(self)
+			X.Banner({
+				Text = "This function can get you banned in up-to-date servers; use at your own risk."
+			})
+		end
+	}
+})
+
+local script8 = Y.Button({
+	Text = "Basketball Pro Simulator AutoShoot/AutoPower",
+	Callback = function()
+		loadstring(game:HttpGet("https://pastebin.com/raw/im0VRNnD"))()
+	end,
+	Menu = {
+		Information = function(self)
+			X.Banner({
+				Text = "This function can get you banned in up-to-date servers; use at your own risk."
+			})
+		end
+	}
+})
+
+local script9 = Y.Button({
+	Text = "DrivingEmpire AutoFarm",
+	Callback = function()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/WindyKung/WindyWare/main/MainLoader.lua"))()
+	end,
+	Menu = {
+		Information = function(self)
+			X.Banner({
+				Text = "This function can get you banned in up-to-date servers; use at your own risk."
+			})
+		end
+	}
+})
+
+local script10 = Y.Button({
+	Text = "Universal Fly/Speed",
+	Callback = function()
+		loadstring(game:HttpGet(('https://raw.githubusercontent.com/Libror/scripts/main/micupgui.lua')))()
+	end,
+	Menu = {
+		Information = function(self)
+			X.Banner({
+				Text = "This function can get you banned in up-to-date servers; use at your own risk."
+			})
+		end
+	}
+})
+
+local script11 = Y.Button({
+	Text = "Royal High AutoFarm (READ INFO) ------>",
+	Callback = function()
+		loadstring(game:HttpGet(('https://pastebin.com/raw/MFi9vGaL')))()
+	end,
+	Menu = {
+		Information = function(self)
+			X.Banner({
+				Text = "Make sure you play in a map that actually has gems (Enchantix)"
+			})
+		end
+	}
+})
+
+local script12 = Y.Button({
+	Text = "Ninja Legends GUI",
+	Callback = function()
+		loadstring(game:HttpGet("https://arches-systems.com/loader.lua",true))()
+	end,
+	Menu = {
+		Information = function(self)
+			X.Banner({
+				Text = "This function can get you banned in up-to-date servers; use at your own risk."
+			})
+		end
+	}
+})
+
+local script13 = Y.Button({
+	Text = "Taxi Boss AutoFarm",
+	Callback = function()
+		loadstring(game:HttpGet(("https://rawscripts.net/raw/Taxi-Boss-OP-Autofarm-4606")))()
+	end,
+	Menu = {
+		Information = function(self)
+			X.Banner({
+				Text = "This function can get you banned in up-to-date servers; use at your own risk."
+			})
+		end
+	}
+})
+
+local script14 = Y.Button({
+	Text = "build a boat autofarmer Gui",
+	Callback = function()
+		loadstring(game:HttpGet('https://raw.githubusercontent.com/1201for/littlegui/main/Build-A-Boat'))()
+	end,
+	Menu = {
+		Information = function(self)
+			X.Banner({
+				Text = "This function can get you banned in up-to-date servers; use at your own risk."
+			})
+		end
+	}
+})
+
+local script15 = Y.Button({
+	Text = "Work at a Pizza Place autofarm",
+	Callback = function()
+		loadstring(game:HttpGet("https://pastebin.com/raw/yMKng0hE",true))()
+	end,
+	Menu = {
+		Information = function(self)
+			X.Banner({
+				Text = "This function can get you banned in up-to-date servers; use at your own risk."
+			})
+		end
+	}
+})
+
+local script16 = Y.Button({
+	Text = "Thief Simulator",
+	Callback = function()
+		loadstring(game:HttpGet("https://www.system-exodus.com/scripts/misc-releases/ThiefSimulator.lua", true))()
+	end,
+	Menu = {
+		Information = function(self)
+			X.Banner({
+				Text = "This function can get you banned in up-to-date servers; use at your own risk."
+			})
+		end
+	}
+})
